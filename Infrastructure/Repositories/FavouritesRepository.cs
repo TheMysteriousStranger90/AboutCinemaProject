68d72fd7c8a7d8e9e64703638fa5812f327cf2bc
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-01-31 15:19:48 +0200

    add FavouritesRepository and start appSerExtensions...

---
+++ Infrastructure/Repositories/FavouritesRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using Core.Entities;
+using Core.Interfaces;
+using StackExchange.Redis;
+
+namespace Infrastructure.Repositories;
+
+public class FavouritesRepository : IFavouritesRepository
+{
+    private readonly IDatabase _database;
+    
+    public FavouritesRepository(IConnectionMultiplexer redis)
+    {
+        _database = redis.GetDatabase();
+    }
+    
+    public async Task<Favourites> GetFavouritesAsync(string favouritesId)
+    {
+        var data = await _database.StringGetAsync(favouritesId);
+
+        return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<Favourites>(data);
+    }
+
+    public async Task<Favourites> UpdateFavouritesAsync(Favourites favourites)
+    {
+        var created = await _database.StringSetAsync(favourites.Id, JsonSerializer.Serialize(favourites),
+            TimeSpan.FromDays(30));
+
+        if (!created) return null;
+
+        return await GetFavouritesAsync(favourites.Id);
+    }
+
+    public async Task<bool> DeleteFavouritesAsync(string favouritesId)
+    {
+        return await _database.KeyDeleteAsync(favouritesId);
+    }
+}
---
+++ WebAPI/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Core.Interfaces;
+using Infrastructure.Context;
+using Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+
+namespace WebAPI.Extensions;
+
+public static class ApplicationServicesExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddSingleton<IResponseCacheService, ResponseCacheService>();
+        
+        services.AddDbContext<AboutCinemaProjectContext>(options =>
+        {
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        });
+        
+        services.AddSingleton<IConnectionMultiplexer>(c => 
+        {
+            var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+            return ConnectionMultiplexer.Connect(options);
+        });
+        
+        
+        
+        
+        
+        
+        services.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy", policy => 
+            {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+            });
+        });
+
+        return services;
+    }
+}
