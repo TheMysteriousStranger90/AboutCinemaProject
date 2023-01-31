820f6c48e6ff83df39422f08469c14d365344cfb
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-01-31 13:47:25 +0200

    add CommentRepository...

--- Core/Interfaces/ICommentRepository.cs
+++ Core/Interfaces/ICommentRepository.cs
@@ -4,5 +4,5 @@ namespace Core.Interfaces;
 
 public interface ICommentRepository
 {
-    Task<IEnumerable<Comment>> GetByMovieIdAsync(int id);
+    Task<IReadOnlyList<Comment>> GetByMovieIdAsync(int id);
 }
---
+++ Infrastructure/Repositories/CommentRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class CommentRepository : ICommentRepository
+{
+    private readonly AboutCinemaProjectContext _context;
+
+    public CommentRepository(AboutCinemaProjectContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<IReadOnlyList<Comment>> GetByMovieIdAsync(int id)
+    {
+        return await _context.Comments.Where(c => c.MovieId == id).AsNoTracking().ToListAsync();
+    }
+}
